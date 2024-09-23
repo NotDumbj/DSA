9c9e8bff1c16c5ed9a12e5617397616d2e6a84e4
NotDumbj <145663898+NotDumbj@users.noreply.github.com>
2024-09-24 00:46:08 +0500

    ~ dsa 1.09 finalized

--- Assignments/Expression Checker 01/libraries/ExpressionHandler.cpp
+++ Assignments/Expression Checker 01/libraries/ExpressionHandler.cpp
@@ -11,7 +11,7 @@ ExpressionHandler::ExpressionHandler(){
 }
 
 void ExpressionHandler::loadData(){
-    std::ifstream expressionFile("../data/expressions.txt");
+    std::ifstream expressionFile("data/expressions.txt");
     
     if(!expressionFile.is_open()){
         isfileReadable = false;
