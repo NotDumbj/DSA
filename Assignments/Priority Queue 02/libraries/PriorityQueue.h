302fd73a5eb68ca51110a98c23768f21ee032895
NotDumbj <145663898+NotDumbj@users.noreply.github.com>
2024-10-04 16:44:22 +0500

    dsa 2.03

--- Assignments/Priority Queue 02/libraries/PriorityQueue.h
+++ Assignments/Priority Queue 02/libraries/PriorityQueue.h
@@ -1,24 +1,32 @@
 #include <iostream>
 #include "Queue.h"
+#include <fstream>
 #include <initializer_list>
+#include <map>
+
 
 template <class itemType>
 class PriorityQueue2D{
     private:
+        std::map<std::string, int> priorityLevels;
         Queue<itemType> *queues;
-        int priorityLevel;
+        int noOfPriorityLevel;
 
     public:
         PriorityQueue2D();
-        PriorityQueue2D(int priorityLevels);
-        PriorityQueue2D(int priorityLevels, std::initializer_list<int> sizes);
+        PriorityQueue2D(int noOfPriorityLevels);
+        PriorityQueue2D(int noOfPriorityLevels, std::initializer_list<int> sizes);
         ~PriorityQueue2D();
+        void readFile();
 };
 
 template <class itemType>
 PriorityQueue2D<itemType>::PriorityQueue2D(){
     priorityLevel = 1;
     queues = new Queue<itemType>[priorityLevel];
+    priorityLevels["Low"] = 0;
+    priorityLevels["Medium"] = 1;
+    priorityLevels["High"] = 2;
 }
 
 template <class itemType>
@@ -37,4 +45,20 @@ PriorityQueue2D<itemType>::PriorityQueue2D(int priorityLevels, std::initializer_
             i++;
         }
     };
+}
+
+template <class itemType>
+void PriorityQueue2D<itemType>::readFile(){
+    path = "Assignments/Priority Queue 02/data/data.txt";
+    std::ifstream inputFile(path);
+    
+    if(!inputFile.is_open()){
+        std::cerr << "Error: File not Opening." << std::endl;
+        return;
+    }
+    
+    std::string lineData;
+    while(std::getline(inputFile, lineData)){
+        
+    }
 }
