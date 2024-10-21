aea99d2bbbcc9791de97a1c3168f8505ceef40a8
NotDumbj <145663898+NotDumbj@users.noreply.github.com>
2024-10-21 12:44:30 +0500

    lab 6 done

---
+++ Labs/Lab 6/Task 1/doublelinkedlist.h
@@ -0,0 +1,27 @@
+typedef struct Node* Nodeptr;
+
+struct Node{
+    int data;
+    Nodeptr next;
+    Nodeptr prev;
+};
+
+class DoubleLinkedList{
+    private:
+        Nodeptr list;
+    public:
+        int length;
+        DoubleLinkedList();
+        void traverse();
+        void traverseR();
+	    Nodeptr find(int);
+        void iAS(int x);
+        int dAS();
+        void iAE(int x);
+        int dAE();
+        int iAM(int x, int index);
+        int dAM(int index);
+        void saveList();
+        void loadList();
+        void clear();
+};
