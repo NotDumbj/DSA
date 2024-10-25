97d2702cf467fd04facddd543ada596063e9d940
NotDumbj <145663898+NotDumbj@users.noreply.github.com>
2024-10-25 05:29:21 +0500

    dsa 4 semi final release

---
+++ Assignments/PQ1D 04/libraries/pq1d.h
@@ -0,0 +1,92 @@
+#include<iostream>
+
+typedef struct Node* Nodeptr;
+
+struct Node{
+    std::string name;
+    std::string role;
+    int priority;
+    Nodeptr next;
+};
+
+
+class PriorityQueue1D{
+    private:
+    Nodeptr head;
+    bool isEmpty();
+    Nodeptr makeNode(std::string name, std::string role, int priority);
+    
+    public:
+    PriorityQueue1D();
+    ~PriorityQueue1D();
+    void insert(std::string name, std::string role, int priority);
+    void display();
+    std::string remove();
+    int getPriority(std::string role);
+};
+
+PriorityQueue1D::PriorityQueue1D(){
+    head = nullptr;
+}
+
+PriorityQueue1D::~PriorityQueue1D(){
+    while(!isEmpty()){
+        remove();
+    }
+}
+
+bool PriorityQueue1D::isEmpty(){
+    return (head == nullptr);
+}
+
+Nodeptr PriorityQueue1D::makeNode(std::string name, std::string role, int priority){
+    Nodeptr newNode = new Node;
+    newNode->name = name;
+    newNode->role = role;
+    newNode->priority = priority;
+    newNode->next = nullptr;
+    return newNode;
+}
+
+void PriorityQueue1D::insert(std::string name, std::string role, int priority){
+    Nodeptr newNode = makeNode(name, role, priority);
+    if(isEmpty() || priority > head->priority){
+        newNode->next = head;
+        head = newNode;
+    }
+    else{
+        Nodeptr current = head;
+        while(current->next!= nullptr && current->next->priority >= priority){
+            current = current->next;
+        }
+        newNode->next = current->next;
+        current->next = newNode;
+    }
+}
+
+void PriorityQueue1D::display(){
+    Nodeptr current = head;
+    std::cout << "Priority Queue: " << std::endl;
+    while(current!= nullptr){
+        std::cout << "Name: " << current->name << ", Role: " << current->role << ", Priority: " << current->priority << std::endl;
+        current = current->next;
+    }
+}
+
+std::string PriorityQueue1D::remove(){
+    if(isEmpty()){
+        throw std::runtime_error("Priority Queue is empty");
+    }
+    std::string name = head->name;
+    Nodeptr temp = head;
+    head = head->next;
+    delete temp;
+    return name;
+}
+
+int PriorityQueue1D::getPriority(std::string role) {
+    if (role == "Teacher") return 2;
+    else if (role == "Parent") return 3;
+    else if (role == "Student") return 1;
+    return 0;
+}
---
+++ Assignments/PQ1D 04/main/main.cpp
@@ -0,0 +1,125 @@
+#include<iostream>
+#include<string>
+#include <Windows.h>
+#undef max
+#include<limits>
+#include<conio.h>
+#include<fstream>
+#include"../libraries/pq1d.h"
+using namespace std;
+ 
+#define filename "data/data.txt"
+
+
+
+bool isRunning = true;
+void userUI(PriorityQueue1D &pQueue);
+void loadData(PriorityQueue1D &pQ);
+
+int main()
+{
+    SetConsoleTitleA("Priority Queue Manager");
+    PriorityQueue1D pQueue;
+    loadData(pQueue);
+    userUI(pQueue);
+    clog << "Exiting..." << endl;
+    return 0;
+}
+
+void loadData(PriorityQueue1D &pQ){
+    ifstream file(filename);
+    if (!file.is_open()) {
+        throw std::runtime_error("Error opening file");
+    }
+
+    Node* pq = nullptr;
+    string role, name;
+    
+    while (file >> role >> ws && getline(file, name)) {
+        int priority = pQ.getPriority(role);
+        pQ.insert(name, role, priority);
+    }
+
+    file.close();
+}
+
+void userUI(PriorityQueue1D &pQueue){
+    cout << "\n" << "PRIORITY QUEUE 1D" << endl;
+    cout << string(15, '=') << endl;
+    cout << "1. Display Items" << endl;
+    cout << "2. Insert" << endl;
+    cout << "3. Remove" << endl;
+    cout << "4. Exit" << endl;
+    cout << string(15, '=') << endl;
+    cout << "Press the desire option number." << endl;
+    char choice;
+    while(isRunning){
+        if(!isRunning){
+            break;
+        }
+        choice = _getch();
+        if(isdigit(choice)){
+            bool isValid = true;
+            string expression = "";
+
+            switch(choice){
+                case '1':
+                    cout << "\nItems: " << endl;
+                    pQueue.display();
+                    userUI(pQueue);
+                    break;
+                case '2':
+                    cout << "\nInserting an Item: " << endl;
+                    cout << "Choose the Priority Level: " << endl;
+                    cout << "1. Student" << endl;
+                    cout << "2. Teacher" << endl;
+                    cout << "3. Parent" << endl;
+                    cout << "=> ";
+                    int priorityLevel;
+                    if (cin >> priorityLevel){
+                        cout << "\nEnter Name: " << endl;
+                        string name;
+                        cin.ignore();
+                        getline(cin, name);
+                        if (priorityLevel == 3){
+                            pQueue.insert(name, "Parent", priorityLevel);
+                        }
+                        else if (priorityLevel == 2){
+                            pQueue.insert(name, "Teacher", priorityLevel);
+                        }
+                        else if (priorityLevel == 1){
+                            pQueue.insert(name, "Student", priorityLevel);
+                        }
+                    }
+                    else{
+                        cin.clear();
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                        cerr << "Error Msg: Invalid input" << endl;
+                        break;
+                    }
+                    userUI(pQueue);
+                    break;
+                case '3':
+                    cout << "\nDeleting... " << endl;
+                    cout << "\nItem deleted: " << pQueue.remove() << endl;
+                    userUI(pQueue);
+                case '4':
+                    isRunning = false;
+                    break;
+                default:
+                    cerr << "Error: Invalid choice." << endl;
+                    userUI(pQueue);
+                    break;
+            }
+            
+        }
+        else{
+            cin.clear();
+            while(_kbhit()){
+                _getch();
+            }
+        }
+    }
+
+
+}
